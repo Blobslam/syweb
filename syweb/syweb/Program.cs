52910f4d6c9b71a37b084d8d9a2af47a48e295ae
Blobslam <philip.tams@gmx.de>
2020-05-28 22:54:06 +0200

    Added base

---
+++ syweb/syweb/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syweb
+{
+    class Program
+    {
+        public static bool SyntaxCorrect = false;
+
+        static void ShowCorrectSyntax()
+        {
+            Console.WriteLine("Available Commands:\n");
+            Console.WriteLine("syweb get <URL <parameters(optional)>> <format>");
+            Console.WriteLine("syweb post <URL <parameters(optional)>> <format> <data>");
+            Console.WriteLine("syweb patch <URL <parameters(optional)>> <format> <data>");
+            Console.WriteLine("syweb put <URL <parameters(optional)>> <format> <data>");
+            Console.WriteLine("Allowed formats are: 'raw' 'json' 'multiencode' leave format empty for using the raw format");
+        }
+
+
+        static void checksyntax(string[] args, string method)
+        {
+
+            if (args[1] == null)
+            {
+                SyntaxCorrect = false;
+                return;
+            }
+
+  
+
+
+            if (method == "POST" || method == "PUT" || method == "patch")
+            {
+                    string format = "";
+                    string data = "";
+                    executeRequest exec = new executeRequest(args[1], method, format, null);
+                    exec.doRequest();
+            }
+            else if(method == "GET")
+            {
+                string format = "";
+                    executeRequest exec = new executeRequest(args[1], method, format, null);
+                    exec.doRequest();
+            }
+            else
+            {
+                Console.WriteLine("The entered method is invalid.");
+                ShowCorrectSyntax();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string method = "";
+            try
+            {
+                switch (args[0])
+                {
+                    case "get":
+                        method = "GET";
+                        checksyntax(args, method);
+                        break;
+                    case "post":
+                        method = "POST";
+                        checksyntax(args, method);
+                        break;
+                    case "patch":
+                        method = "PATCH";
+                        checksyntax(args, method);
+                        break;
+                    case "put":
+                        method = "PUT";
+                        checksyntax(args, method);
+                        break;
+                    // End Main Commands
+                    // Start Help Commands
+                    case "--help":
+                        ShowCorrectSyntax();
+                        break;
+                    case "help":
+                        ShowCorrectSyntax();
+                        break;
+                    case "-help":
+                        ShowCorrectSyntax();
+                        break;
+                    default:
+                        Console.WriteLine("NOPE: Please enter the correct syntax.\n");
+                        ShowCorrectSyntax();
+                        break;
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("\nError: None or too much parameters given.\n");
+                ShowCorrectSyntax();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ syweb/syweb/executeRequest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace syweb
+{
+    class executeRequest
+    {
+        public static string METHOD { get; set; }
+        public static string URL { get; set; }
+        public static string FORMAT { get; set; }
+        public static string DATA { get; set; }
+
+        public executeRequest(string url, string method, string format, string data)
+        {
+            URL = url;
+            METHOD = method;
+            FORMAT = format;
+            DATA = data;
+        }
+
+        public void doRequest()
+        {
+            /*
+            Console.WriteLine("I got the url: " + URL);
+            Console.WriteLine("I got the method: " + METHOD);
+            Console.WriteLine("I got the format: " + FORMAT);
+            Console.WriteLine("I got the data: " + DATA);
+            
+            Console.ReadLine();
+            */
+            //Execute the holy request
+
+
+            WebRequest request = WebRequest.Create(URL);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+
+            if (response.StatusDescription == "OK")
+            {
+                Console.WriteLine("Server responded with OK..");
+                Console.WriteLine("Logging data..\n\n");
+
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(dataStream);
+
+                string responseFromServer = sr.ReadToEnd();
+
+                Console.WriteLine(responseFromServer);
+                sr.Close();
+                dataStream.Close();
+                response.Close();
+                Console.WriteLine("Request Done.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Server did not accept this request.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
